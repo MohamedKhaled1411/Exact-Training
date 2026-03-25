bc80b48b48a6998a18f13b83690cc766a097cc01
Mohamed Khaled <mohamedk123344@gmail.com>
2026-03-25 11:22:49 +0200

    BankAccountVariables

--- src/Session1/BankAccountVariables.java
+++ src/Session1/BankAccountVariables.java
@@ -4,6 +4,17 @@ public class BankAccountVariables
 {
     public static void main (String[] args)
     {
+        String accountHolder = "Mohamed Khaled";
+        long accountNumber = 294203224;
+        double balance = 1634.57;
+        boolean isActive = true;
 
+        String accountStatus;
+        if (isActive)
+            accountStatus = "Active";
+        else
+            accountStatus = "Inactive";
+
+        System.out.printf("%nAccount Holder: %s%nAccount Number: %d%nAccount Balance: %.2f%nAccount Status: %s%n", accountHolder, accountNumber, balance, accountStatus);
     }
 }
