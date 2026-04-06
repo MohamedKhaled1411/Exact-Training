d7b93c01116bc7beefc5e46109b38612ab1bc040
Mohamed Khaled <mohamedk123344@gmail.com>
2026-04-06 15:53:51 +0200

    GradeCalculator

---
+++ src/Session2/GradeCalculator.java
@@ -0,0 +1,48 @@
+package Session2;
+
+import java.util.Scanner;
+
+public class GradeCalculator
+{
+    public static void main(String[] args)
+    {
+        int score;
+        char grade;
+        Scanner input = new Scanner(System.in);
+
+        //score = 30;
+        //score = 50;
+        //score = 75;
+        //score = 85;
+        //score = 95;
+
+        System.out.print("Enter your score: ");
+        score = input.nextInt();
+
+        if (score >= 90)
+        {
+            grade = 'A';
+            System.out.println("Grade: " + grade + "\nExcellent!");
+        }
+        else if (score >= 80)
+        {
+            grade = 'B';
+            System.out.println("Grade: " + grade + "\nExcellent!");
+        }
+        else if (score >= 70)
+        {
+            grade = 'C';
+            System.out.println("Grade: " + grade + "\nGood");
+        }
+        else if (score >= 50)
+        {
+            grade = 'D';
+            System.out.println("Grade: " + grade + "\nGood");
+        }
+        else
+        {
+            grade = 'F';
+            System.out.println("Grade: " + grade + "\nNeeds Improvement");
+        }
+    }
+}
