a9d79d930ab17c25dd2a5053d195fbe5bc77ffd7
Mohamed Khaled <mohamedk123344@gmail.com>
2026-04-06 16:43:19 +0200

    TimeTable

--- src/Session2/TimesTable.java
+++ src/Session2/TimesTable.java
@@ -1,4 +1,18 @@
 package Session2;
 
-public class TimesTable {
+public class TimesTable
+{
+    public static void main (String[] args)
+    {
+        System.out.println("\t\tTable [1]\tTable [2]\tTable [3]\tTable [4]\tTable [5]");
+        for (int i = 1; i <= 12; i++)
+        {
+            System.out.printf("[%d]", i);
+            for (int j = 1; j <= 5; j++)
+                {
+                    System.out.printf("\t\t%dx%d=%d", j, i, i*j);
+                }
+            System.out.println("\t");
+        }
+    }
 }
