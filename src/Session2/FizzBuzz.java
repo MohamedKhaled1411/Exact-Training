40d29687dcd687fdb0a603aeebdd1be64586fe2f
Mohamed Khaled <mohamedk123344@gmail.com>
2026-04-07 11:33:20 +0200

    FizzBuzz

--- src/Session2/FizzBuzz.java
+++ src/Session2/FizzBuzz.java
@@ -1,4 +1,27 @@
 package Session2;
 
-public class FizzBuzz {
+public class FizzBuzz
+{
+    public static void main(String[]args)
+    {
+        for (int i = 1; i <= 50; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                System.out.printf("[%d] FizzBuzz%n", i);
+            }
+            else if (i % 3 == 0)
+            {
+                System.out.printf("[%d] Fizz%n", i);
+            }
+            else if (i % 5 == 0)
+            {
+                System.out.printf("[%d] Buzz%n", i);
+            }
+            else
+            {
+                System.out.printf("[%d]%n", i);
+            }
+        }
+    }
 }
